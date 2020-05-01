1219681c55906f9a4b2d4db0bce058249d935b29
Scott Lyttle <scott.lyttle@gmail.com>
2020-05-01 15:20:52 +0300

    Added build to repo

---
+++ precache-manifest.39b54378045f30cc68be7b7034cf818c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766b8b67a2126ccbe1e45470e74a569c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bc9635dbce961b23b2f",
+    "url": "/static/css/main.cf3a27c7.chunk.css"
+  },
+  {
+    "revision": "883151521e57d5950ee1",
+    "url": "/static/js/2.1a2fdc65.chunk.js"
+  },
+  {
+    "revision": "994dc7263aab8533f921f8fba4d006f2",
+    "url": "/static/js/2.1a2fdc65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc9635dbce961b23b2f",
+    "url": "/static/js/main.293ebc09.chunk.js"
+  },
+  {
+    "revision": "9e4a0023aef98847901d",
+    "url": "/static/js/runtime-main.6592bf6d.js"
+  }
+]);
